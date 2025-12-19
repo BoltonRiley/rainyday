ffbd0b1dc257bc60311dd7bd1697208acc78c48a
BoltonRiley <64459374+BoltonRiley@users.noreply.github.com>
2025-12-19 04:36:56 +0000

    section change and 2D gameplay added

---
+++ scripts/gameLoop.js
@@ -0,0 +1,5 @@
+function startNewGame() {
+    document.getElementById("weatherGate").classList.add("hidden");
+    document.getElementById("characterBuilder").classList.add("hidden");
+    document.getElementById("gameSection").classList.remove("hidden");
+}
